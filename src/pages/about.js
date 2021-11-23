5a1f5e58c4f29199000e3c769ffa2cf63c34274a
frakanoGIT <frakano.7p@gmail.com>
2021-11-23 21:56:47 +0800

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { Link } from "gatsby";
+import * as React from 'react'
+import Layout from '../components/layout'
 const AboutPage = () => {
-    return (
-        <Layout pageTitle="About Me">
-          <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        </Layout>
-      )
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
 }
-
-export default AboutPage;
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import AboutPage from './about';
+import * as React from 'react'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
