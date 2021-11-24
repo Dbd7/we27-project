90bccc3cdccad22dfc5def1690b3e5c7cb76abc4
frakanoGIT <frakano.7p@gmail.com>
2021-11-24 14:35:54 +0800

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "WANGEMAS27 SITE",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
