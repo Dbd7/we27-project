f99eb54aa178c3b240942c2246bb35f27f647a74
frakanoGIT <frakano.7p@gmail.com>
2021-12-01 22:03:43 +0800

    new commit

--- src/pages/gamelist.js
+++ src/pages/gamelist.js
@@ -6,14 +6,14 @@ import {} from "./gamelist.module.css"
 
 const GamelistPage = () => {
     const games = [
-        {name:'918kiss', link: 'http://c1.d.918kiss.com/', icon: <StaticImage alt='whatsapp' src='../images/KS.png' />},
-        {name:'mega888', link: 'https://m.aphqw.com/mega/index.html', icon: <StaticImage alt='telegram' src='../images/MG.png' />},
-        {name:'pussy888', link: 'http://md.pussy888.com/', icon: <StaticImage alt='whatsapp' src='../images/PY.png' />},
-        {name:'xe88', link: 'https://d.playalotgame.com/', icon: <StaticImage alt='telegram' src='../images/XE.png' />},
-        {name:'evo888', link: 'http://d3.evo288.com/', icon: <StaticImage alt='whatsapp' src='../images/EV.png' />},
-        {name:'wuha888', link: 'http://d.wuha888.net/', icon: <StaticImage alt='telegram' src='../images/WH.png' />},
-        {name:'joker', link: 'https://www.joker123.net/', icon: <StaticImage alt='whatsapp' src='../images/JK.png' />},
-        {name:'newtown', link: 'http://www.nbig33.com', icon: <StaticImage alt='telegram' src='../images/NT.png' />},
+        {name:'918kiss', link: 'http://c1.d.918kiss.com/', icon: <StaticImage alt='918kiss' src='../images/icon/KS.png' />},
+        {name:'mega888', link: 'https://m.aphqw.com/mega/index.html', icon: <StaticImage alt='mega888' src='../images/icon/MG.png' />},
+        {name:'pussy888', link: 'http://md.pussy888.com/', icon: <StaticImage alt='pussy888' src='../images/icon/PY.png' />},
+        {name:'xe88', link: 'https://d.playalotgame.com/', icon: <StaticImage alt='xe88' src='../images/icon/XE.png' />},
+        {name:'evo888', link: 'http://d3.evo288.com/', icon: <StaticImage alt='evo888' src='../images/icon/EV.png' />},
+        {name:'wuha888', link: 'http://d.wuha888.net/', icon: <StaticImage alt='wuha888' src='../images/icon/WH.png' />},
+        {name:'joker', link: 'https://www.joker123.net/', icon: <StaticImage alt='joker' src='../images/icon/JK.png' />},
+        {name:'newtown', link: 'http://www.nbig33.com', icon: <StaticImage alt='newtown' src='../images/icon/NT.png' />},
     ]
 
     return (
